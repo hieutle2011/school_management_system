254fad9602e1fdc4b4374e20094e4a63c8d3461e
hieu <hieuletrung102@gmail.com>
2020-07-10 15:24:54 +0700

    feat(middleware): add error handler and authorize

---
+++ src/middleware/index.js
@@ -0,0 +1,42 @@
+
+const jwt = require('express-jwt');
+const config = require('../config');
+
+function errorHandler(err, req, res, next) {
+    console.error(err.stack)
+    if (typeof (err) === 'string') {
+        return res.status(400).json({ message: err });
+    }
+
+    if (err.name === 'UnauthorizedError') {
+        return res
+            .status(401)
+            .json({ message: 'Invalid token or token missing' })
+    }
+    return res
+        .status(500)
+        .json({ message: 'Something broke!' })
+}
+
+function authorize(roles = []) {
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    return [
+        jwt({ secret: config.jwt.secret, algorithms: config.jwt.algorithms }),
+
+        (req, res, next) => {
+            if (roles.length && !roles.includes(req.user.role)) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+            next();
+        }
+    ];
+}
+
+module.exports = {
+    errorHandler,
+    authorize,
+};
