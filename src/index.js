becbbf34c288301a0e225924234dd93548323c63
hieu <hieuletrung102@gmail.com>
2020-07-10 13:37:15 +0700

    feat(route): add get user route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const config = require('./config');
 
+const { User } = require('./users')
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/api/v1/users', async (req, res) => {
+    const users = await User.findAll()
+    res.send(users);
 });
 
 app.listen(config.server.port, () =>
