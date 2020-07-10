c944b73a2774ef85dea206563199028b9e61bc7e
hieu <hieuletrung102@gmail.com>
2020-07-10 16:10:03 +0700

    fix: add try catch

--- src/users/handler.js
+++ src/users/handler.js
@@ -3,29 +3,42 @@ const { User } = require('./index')
 const config = require('../config')
 
 async function authenticate({ username, password }) {
-    const users = await User.findAll()
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        const token = jwt.sign({ id: user.id, role: user.role }, config.jwt.secret);
-        const { password, ...userWithoutPassword } = user;
-        return {
-            ...userWithoutPassword,
-            token
-        };
-    }
+    try {
+        const users = await User.findAll()
+        const user = users.find(u => u.username === username && u.password === password);
+        if (user) {
+            const token = jwt.sign({ id: user.id, role: user.role }, config.jwt.secret);
+            const { password, ...userWithoutPassword } = user;
+            return {
+                ...userWithoutPassword,
+                token
+            };
+        }
 
-    return null
+        return null
+    } catch (error) {
+        next(error)
+    }
 }
 
 async function login(req, res, next) {
-    console.log(req.body)
-    const data = await authenticate(req.body)
-    data ? res.send(data) : res.status(400).json({ message: 'Username or password is incorrect' })
+    try {
+        const data = await authenticate(req.body)
+        data ? res.send(data) : res.status(400).json({ message: 'Username or password is incorrect' })
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 async function getAll(req, res) {
-    const users = await User.findAll()
-    res.send(users);
+    try {
+        const users = await User.findAll()
+        res.send(users);
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 module.exports = {
