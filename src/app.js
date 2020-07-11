67015806d376c9201484b031fa6d2def13584cda
hieu <hieuletrung102@gmail.com>
2020-07-11 12:44:08 +0700

    refactor(app): split app from index.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require("cors");
+const { authorize, errorHandler } = require('./middleware')
+const { getAll, login } = require('./users/handler')
+const { role } = require('./helper')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/api/v1/login', login)
+app.get('/api/v1/users', authorize(role.Admin), getAll);
+
+app.use((req, res) =>
+    res
+        .status(404)
+        .json({ message: `Cannot ${req.method} API ${req.originalUrl}` })
+);
+
+app.use(errorHandler)
+
+// app.listen(config.server.port, () =>
+//     console.log(`Example app listening on port ${config.server.port}!`),
+// );
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-const express = require('express');
+const http = require('http');
+const db = require('./db');
+const app = require('./app');
 const config = require('./config');
-const cors = require("cors");
-const { authorize, errorHandler } = require('./middleware')
-const { getAll, login } = require('./users/handler')
-const { role } = require('./helper')
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const server = http.createServer(app);
+db.sequelize.sync().then(async () => {
+    server.listen(config.server.port, err => {
+        if (err) {
+            console.log(`Start server error`, err);
+        } else {
+            console.log(`Server is listening on port ${config.server.port}`);
+        }
+    });
+}, (err) => {
+    console.log(err)
 });
-
-app.post('/api/v1/login', login)
-app.get('/api/v1/users', authorize(role.Admin), getAll);
-
-app.use((req, res) =>
-    res
-        .status(404)
-        .json({ message: `Cannot ${req.method} API ${req.originalUrl}` })
-);
-
-app.use(errorHandler)
-
-app.listen(config.server.port, () =>
-    console.log(`Example app listening on port ${config.server.port}!`),
-);
