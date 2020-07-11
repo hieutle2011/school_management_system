152fe0ebecbdc9feadbe186ae16c8483c8f0496f
hieu <hieuletrung102@gmail.com>
2020-07-11 12:38:01 +0700

    feat(db): add postgres db

--- src/config/index.js
+++ src/config/index.js
@@ -5,5 +5,34 @@ module.exports = {
     jwt: {
         secret: 'mysecret',
         algorithms: ['HS256'],
+    },
+    dbURL: process.env.DATABASE_URL || "postgres://user:pass@postgres:5432/db",
+    postgre: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME || "db",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || "postgres",
+        pool: {
+            max: process.env.DB_MAX_CONNECT || 10,
+            min: process.env.DB_MIN_CONNECT || 0,
+            acquire: process.env.DB_ACQUIRE_TIMEOUT || 30000,
+            idle: process.env.DB_IDLE_TIMEOUT || 10000
+        }
+    },
+    postgreMaster: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME || "db",
+        host: process.env.DBM_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || "postgres",
+        pool: {
+            max: process.env.DB_MAX_CONNECT || 10,
+            min: process.env.DB_MIN_CONNECT || 0,
+            acquire: process.env.DB_ACQUIRE_TIMEOUT || 30000,
+            idle: process.env.DB_IDLE_TIMEOUT || 10000
+        }
     }
 }
---
+++ src/db/index.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+// const dbURL = require('../config').dbURL;
+
+const config = require('../config').postgre;
+const configMaster = require('../config').postgreMaster;
+const db = {};
+
+if(config.dialect === "postgres"){
+  require('pg').defaults.parseInt8 = true;
+}
+
+// const sequelize = new Sequelize(dbURL);
+let sequelize = new Sequelize(null, null, null, {
+  dialect: config.dialect,
+  replication: {
+    read: config,
+    write: configMaster
+  },
+  logging: false,
+  pool: config.pool,
+});
+
+let users = sequelize.import('users', require('../users/model'));
+db[users.name] = users;
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
