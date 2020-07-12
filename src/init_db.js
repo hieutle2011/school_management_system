eb8a3f6320e9160a6729cc0d60e443961a639656
hieu <hieuletrung102@gmail.com>
2020-07-12 09:00:53 +0700

    feat(db): init db values

--- src/init_db.js
+++ src/init_db.js
@@ -1,15 +1,24 @@
 const userModel = require('./db').user;
+const schoolModel = require('./db').school;
 
-const { role } = require('./helper')
+const { role, status } = require('./helper');
+
+const user_data = [
+    { username: 'Alice', password: 'pw', role: role.Teacher },
+    { username: 'Bob', password: 'pw', role: role.Owner },
+    { username: 'David', password: 'pw', role: role.Admin },
+];
+
+const school_data = [
+    { name: 'CFVG', paymentType: status.Free, ownerId: 2 },
+    { name: 'NEU', paymentType: status.Paid, ownerId: 2 },
+];
 
 async function initDB() {
     console.log('Initialize data')
-    await userModel.truncate();
-    await userModel.bulkCreate([
-        { username: 'Alice', password: 'pw', role: role.Teacher },
-        { username: 'Bob', password: 'pw', role: role.Owner },
-        { username: 'David', password: 'pw', role: role.Admin },
-    ])
+
+    await userModel.bulkCreate(user_data)
+    await schoolModel.bulkCreate(school_data)
 }
 
 module.exports = initDB;
